b33611406e5c29223cb6720f569bc70154896027
Jason Biliguun <jasonbiliguun@Amgalans-MacBook-Pro.local>
2025-01-14 22:53:54 +0800

    changed getposts populate to get the poster's follower id

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,37 +1,37 @@
-const postModel = require("../models/postSchema")
-const userModel = require("../models/userSchema")
+const postModel = require("../models/postSchema");
+const userModel = require("../models/userSchema");
 
-const createPost = async(req,res)=>{
-    try{
-        const body = req.body
-        const {caption, userId, profileImage} = body
+const createPost = async (req, res) => {
+  try {
+    const body = req.body;
+    const { caption, userId, profileImage } = body;
 
-        const createPost = await postModel.create({
-            "caption": caption,
-            "userId": userId,
-            "profileImage": profileImage
-        })
-        await userModel.findByIdAndUpdate(userId,{
-            $push:{
-                posts: createPost._id,
-            }
-        })
-        res.status(200).send(createPost)
+    const createPost = await postModel.create({
+      caption: caption,
+      userId: userId,
+      profileImage: profileImage,
+    });
+    await userModel.findByIdAndUpdate(userId, {
+      $push: {
+        posts: createPost._id,
+      },
+    });
+    res.status(200).send(createPost);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    }catch(err){
-        console.log(err)
-    }
-}
+const getPost = async (req, res) => {
+  try {
+    const post = await postModel
+      .find()
+      .populate("userId", "username profileImage follower");
+    res.status(200).send(post);
+  } catch (err) {
+    res.status(500).send(err);
+    console.log(err);
+  }
+};
 
-
-const getPost = async (req,res)=>{
-    try{
-        const post = await postModel.find().populate("userId", 'username profileImage')
-        res.status(200).send(post)
-    }catch(err){
-        res.status(500).send(err)
-        console.log(err)
-    }
-}
-
-module.exports = {createPost, getPost}
+module.exports = { createPost, getPost };
--- middleware.js
+++ middleware.js
@@ -1,21 +1,19 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const checkToken = (req,res,next)=>{
-        const authToken = req.headers['authorization']
-        const token = authToken.split(" ")[1]
-        
-        if(!token) res.send({message: "no token in authorization"})
-    
-        const verify = jwt.verify(token, process.env.JWT_PASS)
-        
-        if(verify){
-            req.userId = verify
-            next()
-        }else{
-            res.send({message:"token not found"})
-        }
+const checkToken = (req, res, next) => {
+  const authToken = req.headers["authorization"];
+  const token = authToken.split(" ")[1];
 
-    }
-3
+  if (!token) res.send({ message: "no token in authorization" });
 
-module.exports = checkToken
+  const verify = jwt.verify(token, process.env.JWT_PASS);
+
+  if (verify) {
+    req.userId = verify;
+    next();
+  } else {
+    res.send({ message: "token not found" });
+  }
+};
+
+module.exports = checkToken;
